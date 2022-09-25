160aad232f5a0e293a5a54f4273117a1cd6707fe
Miguel Angel <eagleangel@Mac-Studio-de-Miguel.local>
2022-09-25 01:40:46 -0500

    responsive design/header & footer

--- src/app/components/images/mainimages/mainimages.component.ts
+++ src/app/components/images/mainimages/mainimages.component.ts
@@ -64,7 +64,7 @@ export class MainimagesComponent implements AfterViewInit, OnDestroy {
       this.getImages(page);
       setTimeout(() => {
         this.paginateStyle.setData(page, this.pages);
-      }, 1000);
+      }, 1300);
     });
     this.cdr.detectChanges();
   }
